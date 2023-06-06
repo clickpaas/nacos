b32b84d4748d6b19391f1cf5d07814649b81cd21
tingfeng <carlvine500@163.com>
2023-06-06 10:18:55 +0800

    update for security scan

--- config/src/main/java/com/alibaba/nacos/config/server/utils/YamlParserUtil.java
+++ config/src/main/java/com/alibaba/nacos/config/server/utils/YamlParserUtil.java
@@ -20,6 +20,8 @@ import com.alibaba.nacos.api.exception.NacosException;
 import com.alibaba.nacos.api.exception.runtime.NacosRuntimeException;
 import com.alibaba.nacos.common.utils.CollectionUtils;
 import com.alibaba.nacos.config.server.model.ConfigMetadata;
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.LoaderOptions;
 import org.yaml.snakeyaml.constructor.AbstractConstruct;
 import org.yaml.snakeyaml.constructor.SafeConstructor;
 import org.yaml.snakeyaml.introspector.Property;
@@ -51,7 +53,9 @@ public class YamlParserUtil {
      * @return YAML string.
      */
     public static String dumpObject(Object object) {
-        return new Yaml(new YamlParserConstructor(), new CustomRepresenter()).dumpAsMap(object);
+        LoaderOptions loaderOptions = new LoaderOptions();
+        DumperOptions dumperOptions = new DumperOptions();
+        return new Yaml(new YamlParserConstructor(loaderOptions), new CustomRepresenter(dumperOptions)).dumpAsMap(object);
     }
     
     /**
@@ -64,21 +68,32 @@ public class YamlParserUtil {
      * @return Java object.
      */
     public static <T> T loadObject(String content, Class<T> type) {
-        return new Yaml(new YamlParserConstructor(), new CustomRepresenter()).loadAs(content, type);
+        DumperOptions dumperOptions = new DumperOptions();
+        LoaderOptions loaderOptions = new LoaderOptions();
+        return new Yaml(new YamlParserConstructor(loaderOptions), new CustomRepresenter(dumperOptions)).loadAs(content, type);
     }
     
     public static class YamlParserConstructor extends SafeConstructor {
         
         public static Tag configMetadataTag = new Tag(ConfigMetadata.class);
         
-        public YamlParserConstructor() {
-            super();
+//        public YamlParserConstructor() {
+//            LoaderOptions lOptions = new LoaderOptions();
+//            super(lOptions);
+//        }
+
+        public YamlParserConstructor(LoaderOptions loaderOptions) {
+            super(loaderOptions);
             yamlConstructors.put(configMetadataTag, new ConstructYamlConfigMetadata());
         }
     }
     
     public static class CustomRepresenter extends Representer {
-        
+
+        public CustomRepresenter(DumperOptions options) {
+            super(options);
+        }
+
         @Override
         protected NodeTuple representJavaBeanProperty(Object javaBean, Property property, Object propertyValue,
                 Tag customTag) {
